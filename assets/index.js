13f3ef3ad42900eeb712b8c6993d21df0f34ddc2
John <johndeveloperxx@gmail.com>
2022-10-06 11:59:44 -0300

    commit

---
+++ assets/index.js
@@ -0,0 +1,63 @@
+// Sessions
+const sOne = document.querySelector('.s-one');
+const sTwo = document.querySelector('.s-two');
+const sThree = document.querySelector('.s-three');
+const sFour = document.querySelector('.s-four');
+const sFive = document.querySelector('.s-five');
+const sSix = document.querySelector('.s-six');
+const sSeven = document.querySelector('.s-seven');
+
+function arrowOneL(){
+    sOne.scrollBy(-300, 0);
+}
+
+function arrowOneR(){
+    sOne.scrollBy(300, 0);
+}
+
+function arrowTwoL(){
+    sTwo.scrollBy(-300, 0);
+}
+
+function arrowTwoR(){
+    sTwo.scrollBy(300, 0);
+}
+function arrowThreeL(){
+    sThree.scrollBy(-300, 0);
+}
+
+function arrowThreeR(){
+    sThree.scrollBy(300, 0);
+}
+function arrowFourL(){
+    sFour.scrollBy(-300, 0);
+}
+
+function arrowFourR(){
+    sFour.scrollBy(300, 0);
+}
+
+function arrowFiveL(){
+    sFive.scrollBy(-300, 0);
+}
+
+function arrowFiveR(){
+    sFive.scrollBy(300, 0);
+}
+
+function arrowSixL(){
+    sSix.scrollBy(-300, 0);
+}
+
+function arrowSixR(){
+    sSix.scrollBy(300, 0);
+}
+
+function arrowSevenL(){
+    sSeven.scrollBy(-300, 0);
+}
+
+function arrowSevenR(){
+    sSeven.scrollBy(300, 0);
+}
+
